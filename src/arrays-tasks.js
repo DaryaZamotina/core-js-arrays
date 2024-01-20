315108635ac2811e7c05332db5205b690a8b8982
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-01-20 17:14:58 +0300

    feat: make 13 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -273,8 +273,9 @@ function toStringList(/* arr */) {
  *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
  *   distinct([]) => []
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const res1 = arr.filter((elem, index) => index === arr.indexOf(elem));
+  return res1;
 }
 
 /**
