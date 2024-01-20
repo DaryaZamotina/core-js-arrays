c82e4b44f13d1ab6b82c29619c33367ac0934cc2
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-01-20 17:30:58 +0300

    feat: make 19 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -196,8 +196,12 @@ function isValueEqualsIndex(/* arr */) {
  *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  const arrBegin = arr.slice(0, index);
+  const arrEnd = arr.slice(index);
+  const res1 = arrBegin.concat(item);
+  const res2 = res1.concat(arrEnd);
+  return res2;
 }
 
 /**
@@ -211,8 +215,8 @@ function insertItem(/* arr, item, index */) {
  *    getHead([ 'a', 'b', 'c', 'd'], 3) => [ 'a', 'b', 'c' ]
  *    getHead([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  return arr.slice(0, n - 1);
 }
 
 /**
